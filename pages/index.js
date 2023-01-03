cb83265295be81e812d9449287552f873f692489
jado66 <77120464+jado66@users.noreply.github.com>
2023-01-03 16:52:50 -0700

    Fixed a bug

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
-import Footer from '../components/footer'
+import Footer from '../components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 

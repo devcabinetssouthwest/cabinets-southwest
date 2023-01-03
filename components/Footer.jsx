6509157938a23adf30e4dd37e0f30fc41f5082ab
jado66 <jado66@gmail.com>
2023-01-03 23:30:10 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer(){
+    return(
+        <div className="text-center mt-5">
+            <p>
+                Copyright © 2021 Cabinets Southwest
+            </p>
+        </div>
+        
+    )
+}
---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+import '../styles/globals.css'
+import '../styles/styles.scss'
+import { useEffect } from 'react'
+
+export default function App({ Component, pageProps }) {
+
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []); 
+
+  return <Component {...pageProps} />
+}

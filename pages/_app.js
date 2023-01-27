cee869451410afffc095943fb7a078dab16aeb0b
jado66 <77120464+jado66@users.noreply.github.com>
2023-01-27 15:09:17 -0700

    Added google analytics tag to app

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import '../styles/styles.scss'
 import { useEffect } from 'react'
 import { SessionProvider, getSession } from 'next-auth/react'
 import App from 'next/app'
-
+import Script from 'next/script'
 function MyApp({ Component, pageProps,session }) {
 
   useEffect(() => {
@@ -11,9 +11,27 @@ function MyApp({ Component, pageProps,session }) {
   }, []); 
 
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-423TEM5SWD"/>
+      <Script
+        id='google-analytics'
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-423TEM5SWD', {
+          page_path: window.location.pathname,
+          });
+          `,
+        }}
+       />
+    
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </>
   )
 }
 

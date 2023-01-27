1b5cd01bbaa5f4a17167b2cc4289887ad1a17499
jado66 <77120464+jado66@users.noreply.github.com>
2023-01-27 15:26:23 -0700

    Added gtag to measure page views

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,25 @@ import { useEffect } from 'react'
 import { SessionProvider, getSession } from 'next-auth/react'
 import App from 'next/app'
 import Script from 'next/script'
+import * as gtag from "../lib/gtag"
+import { useRouter } from 'next/router'
+
 function MyApp({ Component, pageProps,session }) {
+
+  const router = useRouter();
+ 
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+ 
+    router.events.on("routeChangeComplete", handleRouteChange);
+ 
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
 
   useEffect(() => {
     require("bootstrap/dist/js/bootstrap.bundle.min.js");
@@ -12,6 +30,20 @@ function MyApp({ Component, pageProps,session }) {
 
   return (
     <>
+      <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}/>
+      <Script id='google-analytics'
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_MEASUREMENT_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
       <SessionProvider session={session}>
         <Component {...pageProps} />
       </SessionProvider>
